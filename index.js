d7cfd95add988632fda51836ed54ffb814cd4eaa
adhitya0403 <darshanalaadhitya@gmail.com>
2025-10-15 13:33:33 +0530

    html response error

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import quoteRouter from "./routes/quoteRoutes.js";
 import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
